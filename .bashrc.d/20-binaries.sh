02768a2d0de7daa571b391ee3b25ff9bd03dad47
nikolaDrljaca <devnikoladr@gmail.com>
2026-05-28 19:07:36 +0200

    (shell): bat support

--- .bashrc.d/20-binaries.sh
+++ .bashrc.d/20-binaries.sh
@@ -8,3 +8,6 @@ power() {
     cat /sys/class/power_supply/BAT1/current_now /sys/class/power_supply/BAT1/voltage_now | xargs | awk '{print $1*$2/1e12 " W"}'
 }
 
+if command -v bat &> /dev/null; then
+    alias cat='bat'
+fi
